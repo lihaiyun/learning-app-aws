eea8f9cb19d5a1f2b78da0fba1865a92465d1db6
Li Haiyun <li_haiyun@nyp.edu.sg>
2024-12-03 19:19:42 +0800

    Use environment variables

--- learning-api/handler.js
+++ learning-api/handler.js
@@ -1,8 +1,19 @@
 exports.hello = async (event) => {
+  let params = event.queryStringParameters;
+  let name = params && params.name ? params.name : "world";
+  
+  const STAGE = process.env.STAGE;
+  const TABLE_NAME_PREFIX = process.env.TABLE_NAME_PREFIX;
+
   return {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
+      message: `Hello ${name}!`,
+      stage: STAGE,
+      tableNamePrefix: TABLE_NAME_PREFIX
+    })
   };
 };
