c865face91e1ea617ef099f0fceba156a6bcbc68
Li Haiyun <li_haiyun@nyp.edu.sg>
2025-01-22 12:38:16 +0800

    getCourseListByDomain

--- learning-api/src/courses/addCourse.mjs
+++ learning-api/src/courses/addCourse.mjs
@@ -1,16 +1,30 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
-import { randomUUID } from 'crypto';
+import crypto from 'crypto';
 
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
+
+function generateId() {
+  // Get the current timestamp in seconds
+  const timestamp = Math.floor(new Date().getTime() / 1000);
+
+  // Convert timestamp to hexadecimal string
+  const hexTimestamp = timestamp.toString(16);
+
+  // Generate random bytes and convert to hexadecimal
+  const hexRandom = crypto.randomBytes(4).toString('hex');
+
+  // Combine the timestamp and random part
+  return `${hexTimestamp}-${hexRandom}`;
+}
 
 export const handler = async (event) => {
   // get data from event body
   const data = JSON.parse(event.body);
   // avoid to use reserved words like name, type, domain, status
   const item = {
-    courseId: randomUUID(),
+    courseId: generateId(),
     courseName: data.courseName,
     courseNameLower: data.courseName.toLowerCase(),
     courseDomain: data.courseDomain,
--- learning-api/src/courses/getCourseListByDomain.mjs
+++ learning-api/src/courses/getCourseListByDomain.mjs
@@ -1,26 +1,25 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
 
 export const handler = async (event) => {
-  // set command parameters
+  // get course domain from path parameters
+  const courseDomain = event.pathParameters.courseDomain;
+
+  // set command parameters for query
   let params = {
-    TableName: process.env.COURSES_TABLE
+    TableName: process.env.COURSES_TABLE,
+    IndexName: 'courseDomainIndex',
+    KeyConditionExpression: 'courseDomain = :courseDomain',
+    ExpressionAttributeValues: { ':courseDomain': courseDomain },
+    ScanIndexForward: false // sort by createdAt (descending)
   };
 
-  // search data if query is provided
-  const query = event.queryStringParameters;
-  if (query && query.search) {
-    const search = query.search.toLowerCase();
-    params.FilterExpression = 'contains(courseNameLower, :search)';
-    params.ExpressionAttributeValues = { ':search': search };
-  }
-
   try {
-    // scan data from DynamoDB
-    const command = new ScanCommand(params);
+    // query data from DynamoDB
+    const command = new QueryCommand(params);
     const response = await docClient.send(command);
 
     // sort the list by createdAt (descending)
@@ -38,7 +37,7 @@ export const handler = async (event) => {
     return {
       statusCode: 500,
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ message: 'Failed to get course list' })
+      body: JSON.stringify({ message: 'Failed to get course list by domain' })
     };
   }
 };
