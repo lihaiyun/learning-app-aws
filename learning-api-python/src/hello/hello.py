354d4d86171ec79415c4116d83f80e0218913f25
Li Haiyun <li_haiyun@nyp.edu.sg>
2025-01-22 19:48:51 +0800

    Update hello.py

--- learning-api-python/src/hello/hello.py
+++ learning-api-python/src/hello/hello.py
@@ -1,10 +1,22 @@
+import os
 import json
 
 def handler(event, context):
+    # get name from query string parameters
+    query = event.get("queryStringParameters", {})
+    name = query.get("name", "World")
+
+    # get stage from environment variables
+    STAGE = os.getenv("STAGE", "dev")
+
+    # return response
     body = {
-        "message": "Go Serverless v4.0! Your function executed successfully!",
+        "message": f"Hello {name}!",
+        "stage": STAGE
     }
-
-    response = {"statusCode": 200, "body": json.dumps(body)}
-
+    response = {
+        "statusCode": 200,
+        "headers": { "Content-Type": "application/json" },
+        "body": json.dumps(body)
+    }
     return response
